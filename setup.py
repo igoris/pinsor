4f9db74e81226b8434d98e87a99037d63950ca78
rssvihla <rssvihla@0065c834-e060-11dd-bce9-bf72c0239d3a>
2009-01-31 22:10:04 +0000

    adding setup.py and modified top level package

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+	name='pinsor',
+	version='0.1',
+	description='IoC container',
+	author='Ryan Svihla',
+	author_email='rssvihla@gmail.com',
+	url='http://code.google.com/p/pinsor',
+	download_url='http://code.google.com/p/pinsor/downloads/list',
+	packages=['pinsor'],
+	
+	long_description="""\
+		pinsor is an IoC container with an emphasis on dependency resolution""",
+	classifiers=[
+		"License :: OSI Approved :: Apache Software License",
+		"Programming Language :: Python",
+		"Development Status :: 4 - Beta",
+		"Intended Audience :: Developers",
+		"Topic :: Software Development :: Libraries :: Python Modules",
+		"Operating System :: OS Independent"
+	],
+	keyrwords = "ioc, dependency injection",
+	license="Apache License 2.0",
+	install_requires=[
+	'setuptools'
+	],
+	)
