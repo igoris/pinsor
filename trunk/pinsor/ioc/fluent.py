a96d0e5fa15ad3f77c32272539fd3fd5dea77047
rssvihla <rssvihla@0065c834-e060-11dd-bce9-bf72c0239d3a>
2009-02-17 05:19:59 +0000

    changed docs and class names

--- trunk/pinsor/ioc/fluent.py
+++ trunk/pinsor/ioc/fluent.py
@@ -6,26 +6,26 @@ from enums import *
 class FluentService(object):
 
 	def __init__(self):
-		self.ComponentModel = None
+		self.GraphNode = None
 				
 	def Named(self, key):
-		self.ComponentModel.Key = key
+		self.GraphNode.Key = key
 		return self
 	
 	def Depends(self, dependtuple):
-		self.ComponentModel.Component.Depends = dependtuple
+		self.GraphNode.Component.Depends = dependtuple
 		return self
 	
 	def LifeStyle(self, lifestyle):
-		self.ComponentModel.Component.LifeStyle = lifestyle
+		self.GraphNode.Component.LifeStyle = lifestyle
 		return self
 		
-class Service(object):
+class Component(object):
 	
 	@staticmethod			
 	def For(cls):
 		fluent  = FluentService()
-		fluent.ComponentModel = ComponentModel(key=cls.__name__, component=Component(classtype=cls, depends=[], lifestyle = LifeStyle.Singleton()) )
+		fluent.GraphNode = GraphNode(key=cls.__name__, component=ComponentModel(classtype=cls, depends=[], lifestyle = LifeStyle.Singleton()) )
 		return fluent
 		
 
