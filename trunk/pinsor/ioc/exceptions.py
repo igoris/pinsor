4cdef86a90451e6375047359320bb2f6e5497de8
rssvihla <rssvihla@0065c834-e060-11dd-bce9-bf72c0239d3a>
2009-03-20 13:26:04 +0000

    added exceptions file

---
+++ trunk/pinsor/ioc/exceptions.py
@@ -0,0 +1,9 @@
+class NotFoundInObjectGraphError(Exception):
+	"""docstring for NotFoundInObjectGraph"""
+	def __init__(self, message):
+		self.message
+		
+	def __str__(self):
+		"""oiutputs message __str__"""
+		return repr(self.message)
+		
