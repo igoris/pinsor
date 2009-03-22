45e68b2fb86c086cc57915d4f15bf9ec3214475e
rssvihla <rssvihla@0065c834-e060-11dd-bce9-bf72c0239d3a>
2009-03-22 20:59:06 +0000

    forgot one file yay git

--- trunk/pinsor/tests/__init__.py
+++ trunk/pinsor/tests/__init__.py
@@ -1,2 +1,3 @@
 from pinsor.tests.stories.container_context import *
 from pinsor.tests.stories.fluent_registration_context import fluent_registration_of_objects_tests
+from pinsor.tests.stories.uniqueset import *
