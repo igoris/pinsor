63e60501d6885c5e31f406a8eb13bbb698234b80
rssvihla <rssvihla@0065c834-e060-11dd-bce9-bf72c0239d3a>
2009-03-29 15:58:51 +0000

    changed version number to build eggs

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup,find_packages
 
 setup(
 	name='pinsor',
-	version='0.5.0',
+	version='0.6.0',
 	description='IoC container',
 	author='Ryan Svihla',
 	author_email='rssvihla@gmail.com',
